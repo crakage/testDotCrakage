3ae321fd308a57584c7459bc98011a2e51b3ce0d
David Voillemin <dvoillemin@dvoillemin-ltm7.internal.salesforce.com>
2021-09-08 21:11:13 +0100

    correct render_template call bis

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template(url_for('index.html'))
+    return render_template('index.html')
 
 
 @app.route('/login')
 def login():
     redirect(url_for('index'))
 
+if __name__ == '__main__':
+    app.run()

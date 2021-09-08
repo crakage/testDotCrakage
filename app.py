2a64fbec8b6aac079c44ab4b73ce05d39e70e808
David Voillemin <dvoillemin@dvoillemin-ltm7.internal.salesforce.com>
2021-09-08 21:03:18 +0100

    correct url_for call

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template(url_for('index.html', 'templates'))
+    return render_template(url_for('index.html'))
 
 
 @app.route('/login')
